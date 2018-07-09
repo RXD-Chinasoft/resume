fd59c09fc19c0776716712bd54d3bfd1fda89bcd
RXD-Chinasoft <renxingdong_ios@163.com>
2018-07-09 16:55:22 +0800

    add charts.jsx and layout.jsx

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Layout, Menu, Breadcrumb, Icon, Button } from 'antd';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './home';
+import Charts from './charts'
 
+const routes = [
+    {
+        path: "/",
+        exact: true,
+        sidebar: Home,
+    },
+    {
+        path: "/chart",
+        sidebar: Charts
+    }
+];
 const { Header, Content, Footer, Sider } = Layout;
 const SubMenu = Menu.SubMenu;
 
@@ -16,64 +30,50 @@ class SiderDemo extends React.Component {
 
     render() {
         return (
-            <Layout>
-                <Header style={{ background: '#fff', padding: 0 }} />
-                <Layout style={{ minHeight: '100vh' }}>
+            <Router>
+                <Layout>
+                    <Header style={{ background: 'black', padding: 0 }} />
+                    <Layout style={{ minHeight: '100vh' }}>
 
-                    <Sider
-                        collapsible
-                        collapsed={this.state.collapsed}
-                        onCollapse={this.onCollapse}
-                    >
-                        <div className="logo" />
-                        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                            <Menu.Item key="1">
-                                <Icon type="pie-chart" />
-                                <span>Option 1</span>
-                            </Menu.Item>
-                            <Menu.Item key="2">
-                                <Icon type="desktop" />
-                                <span>Option 2</span>
-                            </Menu.Item>
-                            <SubMenu
-                                key="sub1"
-                                title={<span><Icon type="user" /><span>User</span></span>}
-                            >
-                                <Menu.Item key="3">Tom</Menu.Item>
-                                <Menu.Item key="4">Bill</Menu.Item>
-                                <Menu.Item key="5">Alex</Menu.Item>
-                            </SubMenu>
-                            <SubMenu
-                                key="sub2"
-                                title={<span><Icon type="team" /><span>Team</span></span>}
-                            >
-                                <Menu.Item key="6">Team 1</Menu.Item>
-                                <Menu.Item key="8">Team 2</Menu.Item>
-                            </SubMenu>
-                            <Menu.Item key="9">
-                                <Icon type="file" />
-                                <span>File</span>
-                            </Menu.Item>
-                        </Menu>
-                    </Sider>
-                    <Layout>
+                        <Sider
+                            collapsible
+                            collapsed={this.state.collapsed}
+                            onCollapse={this.onCollapse}
+                        >
+                            <div className="logo" />
+                            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                                <Menu.Item key="1">
+                                    <Link to="/"><Icon type="home" />
+                                        <span>首页</span></Link>
+                                </Menu.Item>
+                                <Menu.Item key="2">
+                                    <Link to="/chart"><Icon type="pie-chart" />
+                                        <span>报表</span></Link>
+                                </Menu.Item>
+                                <SubMenu
+                                    key="sub1"
+                                    title={<span><Icon type="setting" /><span>管理</span></span>}
+                                >
+                                    <Menu.Item key="11"><Link to="/chart">需求</Link></Menu.Item>
+                                </SubMenu>
+                            </Menu>
+                        </Sider>
+                        <Layout>
 
-                        <Content style={{ margin: '0 16px' }}>
-                            <Breadcrumb style={{ margin: '16px 0' }}>
-                                <Breadcrumb.Item>User</Breadcrumb.Item>
-                                <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                            </Breadcrumb>
-                            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                                Bill is a cat.
-                </div>
-                        </Content>
-                        <Footer style={{ textAlign: 'center' }}>
-                            Ant Design ©2016 Created by Ant UED
-              </Footer>
+                            <Content style={{ margin: '0 16px' }}>
+                                {routes.map((route, index) => (
+                                    <Route
+                                        key={index}
+                                        path={route.path}
+                                        exact={route.exact}
+                                        component={route.sidebar}
+                                    />
+                                ))}
+                            </Content>
+                        </Layout>
                     </Layout>
                 </Layout>
-            </Layout>
-
+            </Router>
         );
     }
 }
