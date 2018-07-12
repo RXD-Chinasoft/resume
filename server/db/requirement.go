726e87eccf96f93643593777e21c87b40ebc500f
RXD-Chinasoft <renxingdong_ios@163.com>
2018-07-12 09:38:16 +0800

    add insert requ method

--- server/db/requirement.go
+++ server/db/requirement.go
@@ -31,5 +31,13 @@ func GetRequirements() ([]Requirement, error) {
 
 func NewRequirement(rqmt Requirement) error {
 	log.Printf("require %v", rqmt)
+	_, err := db.Exec("INSERT INTO requirement (requirement, area, count, saler, dm, priority, english, rqtype, rqstatus, client, salaryscope, challengetarget, resumetarget, turn," + 
+		" teamrange, candidate, contact, interviewaddr, projectaddr, createtime, descrpition) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)", 
+		rqmt.Requirement, rqmt.Area, rqmt.Count, rqmt.Saler, rqmt.Dm, rqmt.Priority, rqmt.English, rqmt.Rqtype, rqmt.Rqstatus, rqmt.Client, rqmt.Salaryscope, rqmt.Challengetarget, rqmt.Resumetarget, 
+		rqmt.Turn, rqmt.Teamrange, rqmt.Candidate, rqmt.Contact, rqmt.Interviewaddr, rqmt.Projectaddr, rqmt.Createtime, pq.Array(rqmt.Descrpition))
+	if err != nil {
+		log.Printf("insert error %s", err)
+		return err
+	}
 	return nil
 }
