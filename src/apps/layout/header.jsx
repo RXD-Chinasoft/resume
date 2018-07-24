93c77ce6f193e5813cc36b33730db54264fe8c72
RXD-Chinasoft <renxingdong_ios@163.com>
2018-07-24 11:23:29 +0800

    add css in common

--- src/apps/layout/header.jsx
+++ src/apps/layout/header.jsx
@@ -15,9 +15,9 @@ class Header extends React.Component {
             //         </Col>
             //     </Row>
             // </div>
-            <div style={{ background: 'black', padding: 0, minHeight: 50 }}>
+            <div style={{ background: '#14539d', padding: 0, minHeight: 50 }}>
 
-                <div style={{  float: 'left', color: 'white', paddingTop:10 }}>Spectacular</div>
+                <div style={{  float: 'left', color: 'white', paddingLeft: 10, fontSize: 27 }}>Spectacular</div>
                 <span style={{ float: 'right', marginRight: 15, paddingTop: 10 }}><Icon type="user" style={{ fontSize: 20, color: '#fff' }} /><span style={{ color: 'white' }}>user</span></span>
                 <span style={{ float: 'right', marginRight: 15, paddingTop: 10 }}><Badge count={5}><Icon type="mail" style={{ fontSize: 20, color: '#fff' }} /></Badge></span>
                 <span style={{ float: 'right', marginRight: 15, paddingTop: 10 }}><Badge count={5}><Icon type="notification" style={{ fontSize: 20, color: '#fff' }} /></Badge></span>
