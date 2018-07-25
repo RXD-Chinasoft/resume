a38dc84e172e879318ec002edea9087f31352ff1
RXD-Chinasoft <renxingdong_ios@163.com>
2018-07-25 09:59:13 +0800

    header.jsx p

--- src/apps/layout/header.jsx
+++ src/apps/layout/header.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-import { Row, Col, Button, Icon, Badge } from 'antd';
+import { Row, Col, Button, Icon, Badge, Popover } from 'antd';
 
+const content = (
+    <div>
+        <p>Content</p>
+        <p>Content</p>
+    </div>
+);
 class Header extends React.Component {
 
     render() {
@@ -17,10 +23,10 @@ class Header extends React.Component {
             // </div>
             <div style={{ background: '#14539d', padding: 0, minHeight: 50 }}>
 
-                <div style={{  float: 'left', color: 'white', paddingLeft: 10, fontSize: 27 }}>Spectacular</div>
+                <div style={{ float: 'left', color: 'white', paddingLeft: 10, fontSize: 27 }}>Spectacular</div>
                 <span style={{ float: 'right', marginRight: 15, paddingTop: 10 }}><Icon type="user" style={{ fontSize: 20, color: '#fff' }} /><span style={{ color: 'white' }}>user</span></span>
-                <span style={{ float: 'right', marginRight: 15, paddingTop: 10 }}><Badge count={5}><Icon type="mail" style={{ fontSize: 20, color: '#fff' }} /></Badge></span>
-                <span style={{ float: 'right', marginRight: 15, paddingTop: 10 }}><Badge count={5}><Icon type="notification" style={{ fontSize: 20, color: '#fff' }} /></Badge></span>
+                <Popover placement="bottomLeft" content={content} title="Title" trigger="click"><span style={{ float: 'right', marginRight: 15, paddingTop: 10 }}><Badge count={5}><Icon type="mail" style={{ fontSize: 20, color: '#fff' }} /></Badge></span></Popover>
+                <Popover placement="bottomLeft" content={content} title="Title" trigger="click"><span style={{ float: 'right', marginRight: 15, paddingTop: 10 }}><Badge count={5}><Icon type="notification" style={{ fontSize: 20, color: '#fff' }} /></Badge></span></Popover>
             </div>
         );
     }
