7c257a732ef812d5060178d7e85d84c433a054a2
RXD-Chinasoft <renxingdong_ios@163.com>
2018-07-25 10:44:23 +0800

    recover pan

--- server/main.go
+++ server/main.go
@@ -3,8 +3,15 @@ package main
 import (
 	_ "resume/server/db"
 	server "resume/server/httphandler"
+	"log"
 )
 
 func main() {
+	defer func() {
+		err := recover()
+		if err != nil {
+			log.Printf("panic : %v", err)
+		}
+	}()
 	server.RouteAndListen()
 }
